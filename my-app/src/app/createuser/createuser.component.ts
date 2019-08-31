fbe8525700bc4ff5ade5bdf2cd284b45e6d8bcaa
Lucas <Lucas.geurtjens@griffithuni.edu.au>
2019-08-31 13:07:32 +1000

    Admin page for creating users

--- my-app/src/app/createuser/createuser.component.ts
+++ my-app/src/app/createuser/createuser.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+const BACKEND_URL = 'http://localhost:3000';
 
 @Component({
   selector: 'app-createuser',
@@ -7,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateuserComponent implements OnInit {
 
-  constructor() { }
+  usernameUser = sessionStorage.getItem('usernameUser');
+  groupsUser = sessionStorage.getItem('groupsUser');
+  statusUser = sessionStorage.getItem('statusUser');
+  groupArr = this.groupsUser.split(',');
+
+  // Creating new user
+  pwdNew: string;
+  emailNew: string;
+  usernameNew: string;
+  statusNew: string;
+  newUser = {};
+  allUsers = [];
+  selectedGroup: string;
+  userNameObj = {}; // For delete function
+
+  constructor(private router: Router, private httpClient: HttpClient) { }
 
   ngOnInit() {
+    this.getAllUsers();
   }
 
+  getAllUsers() {
+    this.httpClient.get(BACKEND_URL + '/getUsers')
+      .subscribe((data: any) => {
+        this.allUsers = data;
+        this.statusNew = 'Regular';
+    });
+  }
+
+  createUser() {
+    this.newUser = {
+      username: this.usernameNew,
+      email: this.emailNew,
+      pwd: this.pwdNew,
+      status: this.statusNew
+    };
+    this.httpClient.post(BACKEND_URL + '/addUser', this.newUser)
+      .subscribe((data: any) => {
+        console.log('success');
+    });
+    this.pwdNew = '';
+    this.emailNew = '';
+    this.usernameNew = '';
+    this.statusNew = '';
+    this.getAllUsers();
+  }
+
+  // Eventually we need to get a callback function in here which can call getAllUsers();
+  deleteUser(userID) {
+    this.userNameObj = {userID};
+    this.httpClient.post(BACKEND_URL + '/deleteUser', this.userNameObj)
+      .subscribe((data: any) => {
+      console.log('success');
+    });
+    this.getAllUsers();
+  }
 }
