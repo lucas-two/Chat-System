7ad66195105708ed0183a43ba1a95279d3fa19a8
Lucas <Lucas.geurtjens@griffithuni.edu.au>
2019-09-23 14:08:15 +1000

    Login working with mongo

--- my-app/server/routes/api-login.js
+++ my-app/server/routes/api-login.js
@@ -1,40 +1,42 @@
-const fs = require('fs');
-
-module.exports = (app) => {
+module.exports = (db,app) => {
   app.post('/login',function(req,res){
 
-  // Debugging
-  console.log('Hit by Angular');
-
-  // Read the data from the JSON file and parse it to users
-  let data = fs.readFileSync('users.json');
-  let users = JSON.parse(data);
-
-  if (!req.body) {
-    return res.sendStatus(400);
-  }
-
-  // Object used to store user (customer) info
-  let customer = {};
-  customer.valid = false;
-  customer.email = '';
-  customer.username = '';
-  customer.status = '';
-  customer.groups = [];
-
-  // For all users in the system
-  for (let i = 0; i < users['users'].length; i++) {
-    // If we find the matching email & password
-    if (req.body.userEmail == users['users'][i].email && req.body.userPwd == users['users'][i].pwd){
-      // Store the user info in an object
-      customer.valid = true;
-      customer.email = users['users'][i].email;
-      customer.username = users['users'][i].username;
-      customer.status = users['users'][i].status;
-      customer.groups = users['users'][i].groups;
-      }
+    // Debugging
+    console.log("api-login hit angular");
+    if (!req.body) {
+      return res.sendStatus(400)
     }
-    // Send the object
-    res.send(customer);
+
+    // Use USERS collection
+    const collection = db.collection('users');
+
+    // Object for storing user info
+    let userObj = {};
+    userObj.valid = false;
+    userObj.email = '';
+    userObj.username = '';
+    userObj.status = '';
+    userObj.groups = [];
+
+    // We want to find the input email and password
+    let query = {'email': req.body.userEmail, 'pwd': req.body.userPwd}
+
+    // Run query
+    collection.findOne(query).then(doc => {
+
+      // Valid user
+      if(doc != null) {
+        userObj.valid = true;
+        userObj.email = doc.email;
+        userObj.username = doc.username;
+        userObj.status = doc.status;
+        userObj.groups = doc.groups;
+        res.send(userObj);
+      }
+      // Invalid user
+      else {
+        res.send(userObj);
+      }
+    });
   });
 }
--- my-app/server/server.js
+++ my-app/server/server.js
@@ -1,28 +1,51 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 
+const MongoClient = require('mongodb').MongoClient;
+var  ObjectID = require('mongodb').ObjectID;
+
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(express.static('http://localhost:4200'));
+app.use(bodyParser.urlencoded({extended: true})); // old
+app.use (bodyParser.json());
 
-require('./listen.js')(http);
-require('./routes/api-login.js')(app);
-require('./routes/api-add-user.js')(app);
-require('./routes/api-get-users.js')(app);
-require('./routes/api-delete-user.js')(app);
-require('./routes/api-update-status.js')(app);
-require('./routes/api-get-groups.js')(app);
-require('./routes/api-add-to-group.js')(app);
-require('./routes/api-add-to-channel.js')(app);
-require('./routes/api-remove-from-group.js')(app);
-require('./routes/api-remove-from-channel.js')(app);
-require('./routes/api-add-group.js')(app);
-require('./routes/api-add-channel.js')(app);
-require('./routes/api-delete-group.js')(app);
-require('./routes/api-delete-channel.js')(app);
+const url = 'mongodb://localhost:27017';
 
+MongoClient.connect(url, {poolSize:10,useNewUrlParser: true,useUnifiedTopology: true},function(err, client) {
+    if (err) {
+        return console.log(err)
+    }
+    const dbName = 'chat';
+    const db = client.db(dbName);
+
+    require('./listen.js')(http);
+    require('./routes/api-login.js')(db,app);
+
+    // require('./routes/api-add.js') (db,app);
+    // require('./routes/api-get.js') (db,app);
+    // require('./routes/api-update.js') (db,app,ObjectID);
+    // require('./routes/api-remove.js') (db,app,ObjectID);
+
+
+
+});
+
+
+
+// require('./listen.js')(http);
+//
+// require('./routes/api-add-user.js')(app);
+// require('./routes/api-get-users.js')(app);
+// require('./routes/api-delete-user.js')(app);
+// require('./routes/api-update-status.js')(app);
+// require('./routes/api-get-groups.js')(app);
+// require('./routes/api-add-to-group.js')(app);
+// require('./routes/api-add-to-channel.js')(app);
+// require('./routes/api-remove-from-group.js')(app);
+// require('./routes/api-remove-from-channel.js')(app);
+// require('./routes/api-add-group.js')(app);
+// require('./routes/api-add-channel.js')(app);
+// require('./routes/api-delete-group.js')(app);
+// require('./routes/api-delete-channel.js')(app);
